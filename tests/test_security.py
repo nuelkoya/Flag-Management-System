31f68b10b02e36a46936e7f56ef1f0e37a333290
Adetola <Adetola442@gmail.com>
2026-02-12 21:04:44 -0600

    test: implement unit test for get_password_hash

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,6 +1,9 @@
+from unittest.mock import MagicMock, patch
 import pytest
 from pwdlib import PasswordHash
-from security import verify_password
+from security import verify_password, get_password_hash
+
+
 hasher = PasswordHash.recommended()
 FAKE_PWD = "Test1P@ss"
 FAKE_HASH = hasher.hash(FAKE_PWD)
@@ -13,6 +16,7 @@ def test_verify_password():
     )
     assert result is True
 
+
 @pytest.mark.parametrize("password,hashed_password", [
     (FAKE_PWD, DUMMY_HASH), ("FakePass!1", FAKE_HASH)
 ])
@@ -22,4 +26,20 @@ def test_verify_password_invalid(password, hashed_password):
         hashed_password = hashed_password
     )
     assert result is False
+
+
+def test_get_password_hash():
+    result = get_password_hash(
+        password=FAKE_PWD
+    )
+    assert "$argon" in result
+
+
+def test_get_password_hash_empty():
+    result = get_password_hash(
+        password=""
+    )
+    assert "$argon" in result
+
+
 
